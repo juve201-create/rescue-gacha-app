ce7a30d384227d94e21eceeb93cd39e9588779b0
juve201-create <juve201@gmail.com>
2026-05-25 11:28:07 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,30 @@
-const CACHE = 'rescue-gacha-v2';
+const CACHE = 'rescue-gacha-v3';
 const ASSETS = ['/', '/index.html', '/icon.jpg', '/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => {
-      return cached || fetch(e.request);
-    })
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
